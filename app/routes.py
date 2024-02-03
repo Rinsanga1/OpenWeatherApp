35823091a0619e01912039df8d7a2846a10e08b8
Rinsanga1 <rinsanga1221@gmail.com>
2024-02-03 19:23:59 +0530

    little stuff

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,23 @@
-from flask import Flask, json,render_template,request,jsonify
+from flask import Flask, json, render_template, request, jsonify
 from app.search_weather import search_weather
 from app.forms import City_Form
-from app import app,db
+from app import app, db
 from app.models import CSC
 import sqlalchemy as sa
 
 app.app_context().push()
 cities = db.session.scalars(sa.select(CSC.city)).all()
 
-@app.route('/', methods = ('GET','POST'))
+
+@app.route('/', methods=('GET', 'POST'))
 def weather():
     form = City_Form()
     weather_data = None
     if request.method == 'POST':
-        weather,temperature, humidity = search_weather(form.city_form.data)
-        weather_data = { 'weather': weather , 'temperature': temperature, 'humidity': humidity }
-    return render_template('base.html', form = form , weather_data=weather_data)
+        weather, temperature, humidity = search_weather(form.city_form.data)
+        weather_data = {'weather': weather,
+                        'temperature': temperature, 'humidity': humidity}
+    return render_template('base.html', form=form, weather_data=weather_data)
 
 
 @app.route('/suggestion')
--- openweather.py
+++ openweather.py
@@ -1,8 +1,6 @@
 from app import app,db
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app.models import CSC
 
-@app.shell_context_processor
-def make_shell_context():
-    return { 'sa':sa , 'so':so,'db':db,'CSC':CSC }
+
+
+
+
