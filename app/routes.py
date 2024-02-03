f919f0781ca1ea6a4038b2e0cb5d867704f9e317
Rinsanga1 <rinsanga1221@gmail.com>
2024-02-03 14:19:25 +0530

    made suggestion for searching cities

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template,request
+from flask import Flask, json,render_template,request,jsonify
 from app.search_weather import search_weather
 from app.forms import City_Form
-from app import app
+from app import app,db
+from app.models import CSC
+import sqlalchemy as sa
 
-
+app.app_context().push()
+cities = db.session.scalars(sa.select(CSC.city)).all()
 
 @app.route('/', methods = ('GET','POST'))
 def weather():
@@ -13,3 +16,11 @@ def weather():
         weather,temperature, humidity = search_weather(form.city_form.data)
         weather_data = { 'weather': weather , 'temperature': temperature, 'humidity': humidity }
     return render_template('base.html', form = form , weather_data=weather_data)
+
+
+@app.route('/suggestion')
+def suggest_city():
+    term = request.args.get('term')
+    suggestions = [city for city in cities if term.lower() in city.lower()]
+
+    return jsonify(suggestions)
