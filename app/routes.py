ea1993616607f05483026f3d67a8ceb6dd440f2b
Rinsanga1 <rinsanga1221@gmail.com>
2024-02-12 00:00:23 +0530

    css hard

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app.search_weather import search_weather
 from app.forms import City_Form
 from app.models import CSC
 
+
 app.app_context().push()
 cities = db.session.scalars(sa.select(CSC.city)).all()
 
@@ -13,13 +14,16 @@ cities = db.session.scalars(sa.select(CSC.city)).all()
 def weather():
     form = City_Form()
     weather_data = None
+
     if request.method == 'POST':
         sunrise,sunset,temp,humidity,wether,windspeed,feelslike,icon,ldt,state,country = search_weather(form.city_form.data)
+
         weather_data = {'state': state, 'country': country , 
                         'weather': wether,'sunrise': sunrise, 'sunset': sunset,
                         'temperature': temp, 'humidity': humidity , 
                         'windspeed' : windspeed , 'feelslike': feelslike ,
                         'icon': icon, 'localdt':ldt}
+
     return render_template('base.html', form=form, weather_data=weather_data)
 
 
@@ -27,5 +31,6 @@ def weather():
 def suggest_city():
     term = request.args.get('term')
     suggestions = [city for city in cities if term.lower() in city.lower()]
+
     return jsonify(suggestions)
 
--- app/search_weather.py
+++ app/search_weather.py
@@ -5,42 +5,50 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import os
 
+
 load_dotenv()
 api_key = os.getenv('API_KEY')
 
 
 def search_weather(cityi):
     buffer = CSC.query.filter_by(city=cityi).first()
+
     lat = buffer.lat
     lon = buffer.lon
     state = buffer.state
     country = buffer.country
+
     sunrise, sunset, temp, humidity, wether, windspeed, feelslike,icon,ldt = weather(
         lat, lon, api_key)
+
     return sunrise, sunset, temp, humidity, wether, windspeed, feelslike,icon,ldt, state, country
 
 
 def weather(lat, lon, api_key):
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
     response = requests.get(url).json()
+
     weather = response['weather'][0]['main']
     icon = response['weather'][0]['icon']
     temp = int(response['main']['temp'] - 273.15)
     humidity = response['main']['humidity']
     windspeed = int(response['wind']['speed'])
     feelslike = int(response['main']['feels_like'] - 273.15)
+
     sunrisets, sunsetts, offset = response['sys']['sunrise'], response['sys']['sunset'], response['timezone']
     sunrise, sunset, localdt = sunrise_sunset(sunrisets, sunsetts, offset)
+
     return sunrise, sunset, temp, humidity, weather, windspeed, feelslike, icon,localdt
 
 
 def sunrise_sunset(sunrisets, sunsetts, offset):
-    def convert_timestamp_to_timezone(timestamp, timezone_offset):
-        return datetime.utcfromtimestamp(timestamp + timezone_offset)
-    sunrise_datetime = convert_timestamp_to_timezone(sunrisets, offset)
-    sunset_datetime = convert_timestamp_to_timezone(sunsetts, offset)
+    sunrise_datetime = datetime.utcfromtimestamp( sunrisets + offset)
+    sunset_datetime = datetime.utcfromtimestamp( sunsetts + offset)
+
     offset_datetime = datetime.utcnow() + timedelta(seconds=offset)
+
     sunrise_str = sunrise_datetime.strftime("%-I:%M %p %Z%z")  
     sunset_str = sunset_datetime.strftime("%-I:%M %p %Z%z")  
     offset_str = offset_datetime.strftime("%Y/%m/%d %-I:%M %p %Z%z")
+
     return sunrise_str, sunset_str, offset_str
